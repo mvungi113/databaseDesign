87aa959a416ed1d850254fa7be5d9ecba46fa5f5
devsTz <rogashianmvungi@gmail.com>
2024-04-25 20:04:38 +0300

    In this branch i was demostrate on how to insert data in customer table

---
+++ insertingData/inser_data_customer_table.sql
@@ -0,0 +1,28 @@
+
+USE restaurantdb;
+
+
+-- insert data in customer table
+INSERT INTO customer(customerID, customerName, customerEmail, phoneNumber, customerAddress) VALUES
+-- You can insert more than one value here
+	 (NULL,'Rogasian Haji','mvungi@email.com','0769657607', 'P.O BOX 131, mbeya'),
+	 (NULL,'Gamba Luchu','gamba_luchu@gmail.com','0769657688', 'P.O BOX 131, mbeya'),
+	 (NULL,'Mbuzi Haji','mbuzi@haji.com','0769657609', 'P.O BOX 131, mbeya'),
+	 (NULL,'Chacha Mgeta','mgeta@email.com','0769657093', 'P.O BOX 1031, Makambako'),
+	 (NULL,'Nzullo Dee','deenzullo@gmail.com','0769657645', 'P.O BOX 1301, Iringa'),
+ 	 (NULL,'Mahaba Sultan','sultanmahaba@email.com','0769057633', 'P.O BOX 1311, Kilimanjaro'),
+    (NULL, 'Rogasian Haji', 'mvungi@email.com', '0769657633', 'P.O , Mbeya'),
+    (NULL, 'John Doe', 'john.doe@example.com', '1234567890', '123 , Cityville'),
+    (NULL, 'Jane Smith', 'jane.smith@example.com', '0987654321', '456, Townsville'),
+    (NULL, 'Alice Johnson', 'alice.johnson@example.com', '1122334455', '789, Villageville'),
+    (NULL, 'Bob Brown', 'bob.brown@example.com', '5566778899', '101, Hamletville'),
+    (NULL, 'Charlie Davis', 'charlie.davis@example.com', '2233445566', '202, Settlementville'),
+    (NULL, 'Diana Evans', 'diana.evans@example.com', '3344556677', '303, Countryside'),
+    (NULL, 'Eve Foster', 'eve.foster@example.com', '4455667788', '404, Metropolis'),
+    (NULL, 'Frank Green', 'frank.green@example.com', '5566778890', '505, Suburbia'),
+    (NULL, 'Grace Hall', 'grace.hall@example.com', '6677889901', '606, Urbanville'),
+    (NULL, 'Hannah Ingle', 'hannah.ingle@example.com', '7788990012','707, Uptown'),
+    (NULL, 'Ian Jones', 'ian.jones@example.com', '8899001123', '808, Downtown'),
+    (NULL, 'Jack King', 'jack.king@example.com', '9900112234', '909, Midtown'),
+    (NULL, 'Karen Lee', 'karen.lee@example.com', '0011223345', '1010, Capitol'),
+    (NULL, 'Larry Miller', 'larry.miller@example.com', '1122334456', '1111, Riverside');
